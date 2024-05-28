a4a5bc660e502658eb6b02461cde6b474bfc41a1
mohamadmarandi <mohamadmarandi83@gmail.com>
2024-05-28 14:25:10 +0330

    adding currentPlayer and opposition fields and currentGame static field

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/Game.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/Game.java
@@ -6,15 +6,37 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Game {
+    private static Game currentGame;
     private ArrayList<User> players;
     private Date date;
     private ArrayList<Round> rounds;
     private GameBoard gameBoard;
+    private Player currentPlayer;
+    private Player opposition;
 
-    public Game() {
+    public Game(Player player, Player opposition) {
         players = new ArrayList<User>();
         date = new Date();
         rounds = new ArrayList<Round>();
         gameBoard = new GameBoard();
+        currentGame = this;
+        this.currentPlayer = player;
+        this.opposition = opposition;
+    }
+
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        Game.currentGame = currentGame;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public Player getOpposition() {
+        return opposition;
     }
 }
--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/PlayableCard.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/PlayableCard.java
@@ -1,6 +1,6 @@
 package com.mygdx.game.model;
 
-public abstract class PlayableCard extends AbstractCard {
+public class PlayableCard extends AbstractCard {
     public PlayableCard(String name, String description) {
         super(name, description);
     }
