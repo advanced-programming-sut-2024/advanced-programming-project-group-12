3df81cb8189c0c1dc0b1fffd3aa8fb2c19a1b671
radical-1 <sym.log.1.12@gmail.com>
2024-07-03 13:10:45 +0330

    merge commit

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/view/screen/GameScreen.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/view/screen/GameScreen.java
@@ -20,7 +20,7 @@ import com.mygdx.game.model.Game;
 import com.mygdx.game.model.Player;
 import com.mygdx.game.model.actors.*;
 import com.mygdx.game.model.card.AbstractCard;
-import com.mygdx.game.model.gameBoard.GameBoard;
+import com.mygdx.game.model.GameBoard;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
