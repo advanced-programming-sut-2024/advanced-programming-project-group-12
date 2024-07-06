3aba8bd09b4072476482a532aa74c9e3ec12c5e8
mohamadmarandi <mohamadmarandi83@gmail.com>
2024-07-06 10:18:48 +0330

    changing discard to use strings

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/game/card/Discard.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/game/card/Discard.java
@@ -3,8 +3,8 @@ package com.mygdx.game.model.game.card;
 import java.util.ArrayList;
 
 public class Discard {
-    private ArrayList<PlayableCard> playableCards;
-    private ArrayList<AbstractCard> cards;
+    private ArrayList<String> playableCards;
+    private ArrayList<String> cards;
     public Discard() {
         cards = new ArrayList<>();
         playableCards = new ArrayList<>();
@@ -15,17 +15,25 @@ public class Discard {
     }
 
     public void addCard(AbstractCard abstractCard) {
-        cards.add(abstractCard);
+        cards.add(abstractCard.getName());
         if(abstractCard instanceof PlayableCard) {
-            playableCards.add((PlayableCard) abstractCard);
+            playableCards.add(abstractCard.getName());
         }
     }
 
     public ArrayList<AbstractCard> getDiscardCards() {
-        return cards;
+        ArrayList<AbstractCard> discardCards = new ArrayList<>();
+        for(String i: cards) {
+            discardCards.add(AllCards.getCardByCardName(i));
+        }
+        return discardCards;
     }
 
     public ArrayList<PlayableCard> getPlayableCards() {
+        ArrayList<PlayableCard> playableCards = new ArrayList<>();
+        for(String i: cards) {
+            playableCards.add((PlayableCard) AllCards.getCardByCardName(i));
+        }
         return playableCards;
     }
 }
