eecd4c0197a8d12429865448918c605850c16b66
radical-1 <sym.log.1.12@gmail.com>
2024-07-08 15:08:49 +0330

    game progress

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/controller/local/GameController.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/controller/local/GameController.java
@@ -1,11 +1,13 @@
 package com.mygdx.game.controller.local;
 
 
+import com.mygdx.game.Gwent;
 import com.mygdx.game.model.game.Game;
 import com.mygdx.game.model.game.card.*;
 import com.mygdx.game.model.network.Client;
 import com.mygdx.game.model.user.Player;
 import com.mygdx.game.model.game.GameBoard;
+import com.mygdx.game.view.screen.GameScreen;
 
 import java.util.ArrayList;
 
@@ -20,66 +22,6 @@ public class GameController {
     public AbstractCard getSelectedCard() {
         return selectedCard;
     }
-    public void playCard(PlayableCard card) {
-        // Play a card
-
-    }
-    public void vetoCard(int cardNumber) {
-        // Veto a card
-    }
-
-    public void inHandDeck(int cardNumber) {
-
-    }
-
-    public void remainingCardsToPlay() {
-
-    }
-
-    public void outOfPlayCards() {
-
-    }
-
-    public void cardsInRow(int rowNumber) {
-
-    }
-
-    public void spellsInPlay() {
-
-    }
-
-    public void putCard(int cardNumber, int rowNumber) {
-
-    }
-
-    public void CommanderInfo() {
-
-    }
-
-    public void PlayersInfo() {
-
-    }
-
-    public void PlayersLivesInfo() {
-
-    }
-
-    public void NumberOfCardsInHand() {
-
-    }
-
-    public void TurnInfo() {
-
-    }
-
-    public void totalScore() {
-
-    }
-
-    public void totalScoreOfRow(int rowNumber) {
-
-    }
-
     public void passRound() {
 
     }
@@ -125,12 +67,13 @@ public class GameController {
     }
 
     /*
-    in this method i assume that player is in select mode
-    and i give you card's that is selected and you should handle it
+    in this method I assume that player is in select mode
+    and I give you card's that is selected and you should handle it
      */
     public void chooseCardInSelectCardMode(ArrayList<AbstractCard> cards) {
         //TODO :
     }
+
     public void setGame(Game game) {
         this.game = game;
     }
@@ -138,9 +81,6 @@ public class GameController {
         return game;
     }
 
-    public void playLeader(CommanderCard leader) {
-        //Play leader
-    }
 
     public void chooseWhichPlayerStartFirst(String username) {
 
@@ -149,4 +89,8 @@ public class GameController {
     public void goToMainMenu() {
 
     }
+    public void chooseStarter() {
+        ((GameScreen)Gwent.singleton.getCurrentScreen()).showChooseStarter();
+    }
+
 }
