5d6b1f5d4ea7c6616c6635ccb8b2e646235e4676
radical-1 <sym.log.1.12@gmail.com>
2024-07-10 18:54:59 +0330

    make chat

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/view/screen/GameScreen.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/view/screen/GameScreen.java
@@ -65,12 +65,12 @@ public class GameScreen implements Screen {
     private Player player;
     private Player opposition;
     //chat parts
-    private ChatBox chatBox;
     private TextButton chatButton;
 
     public GameScreen() {
         controller = new GameController();
         stage = new Stage(new ScreenViewport());
+        new ChatUI(stage, Gwent.singleton.skin);
         background = new Texture("bg/board.jpg");
         if (Client.getInstance().getGame().getCurrentPlayer().getUsername().equals(Client.getInstance().getUser().getUsername())) {
             player = Client.getInstance().getGame().getCurrentPlayer();
@@ -158,10 +158,10 @@ public class GameScreen implements Screen {
         chatButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                if (chatBox.isVisible()) {
-                    chatBox.hide();
+                if (ChatUI.getInstance().getRootTable().isVisible()) {
+                    ChatUI.getInstance().hide();
                 } else {
-                    chatBox.show();
+                    ChatUI.getInstance().show();
                 }
             }
         });
@@ -176,20 +176,6 @@ public class GameScreen implements Screen {
         displayWeatherBox();
         stage.addActor(weatherBox);
 
-        //chat box
-        chatBox = new ChatBox(Gwent.singleton.skin);
-        chatBox.setPosition(400, 500); // set the position of the chat box
-        chatBox.getSendButton().addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                String message = chatBox.getInputText();
-                if (message.isEmpty()) return true;
-                ChatController.sendMessage(Client.getInstance().getUser().getUsername(), message);
-                chatBox.clearInput();
-                return true;
-            }
-        });
-        stage.addActor(chatBox);
 
         //discards
         playerDiscards = new Container<Actor>();
@@ -760,6 +746,7 @@ public class GameScreen implements Screen {
         showTurn = true;
         stage.clear();
         initialStageObjects();
+        ChatUI.getInstance().returnToStage(stage);
     }
 
     public void endGame(int state) {
