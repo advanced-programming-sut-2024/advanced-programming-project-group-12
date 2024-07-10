2a4612e0530dc933af0eb160b9dbeb4f74b8adf3
radical-1 <sym.log.1.12@gmail.com>
2024-07-10 21:33:50 +0330

    debug

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/actors/Message.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/model/actors/Message.java
@@ -5,6 +5,17 @@ public class Message {
     private String Sender;
     private String Message;
     private String time;
+    private String replyToSender;
+    private String replyToMessage;
+
+    public Message(boolean isSender, String sender, String message, String time, String replyToSender, String replyToMessage) {
+        this.isSender = isSender;
+        Sender = sender;
+        Message = message;
+        this.time = time;
+        this.replyToSender = replyToSender;
+        this.replyToMessage = replyToMessage;
+    }
 
     public boolean isSender() {
         return isSender;
@@ -29,17 +40,4 @@ public class Message {
     public String getReplyToMessage() {
         return replyToMessage;
     }
-
-    private String replyToSender;
-    private String replyToMessage;
-
-    public Message(boolean isSender, String sender, String message, String time, String replyToSender, String replyToMessage) {
-        this.isSender = isSender;
-        Sender = sender;
-        Message = message;
-        this.time = time;
-        this.replyToSender = replyToSender;
-        this.replyToMessage = replyToMessage;
-    }
-
 }
