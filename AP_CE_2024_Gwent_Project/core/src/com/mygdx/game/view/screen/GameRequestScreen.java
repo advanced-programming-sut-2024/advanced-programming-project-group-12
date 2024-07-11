d1cfd1ff27a0bd8e52e2a28747b1bf975104154a
ali1556 <ali.nouri@g2holding.org>
2024-07-11 19:09:07 +0330

    added a condition

--- AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/view/screen/GameRequestScreen.java
+++ AP_CE_2024_Gwent_Project/core/src/com/mygdx/game/view/screen/GameRequestScreen.java
@@ -85,7 +85,18 @@ public class GameRequestScreen implements Screen {
         sendAGameRequestButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                if (!requestSent) {
+                String username = nameField.getText();
+                if (!userIsMyFriend(username)) {
+                    Dialog dialog = new Dialog("Error", Gwent.singleton.skin) {
+                        public void result(Object obj) {
+                        }
+                    };
+                    dialog.row();
+                    dialog.text("You can only send game requests to you friends").row();
+                    dialog.button("OK", true);
+                    dialog.show(stage);
+                }
+                else if (!requestSent) {
                     String name = nameField.getText();
                     if (controller.userExists(name)) {
                         String sender = User.getLoggedInUser().getUsername();
@@ -118,6 +129,10 @@ public class GameRequestScreen implements Screen {
                 errorLabel.setText("");
             }
         });
+    }
+
+    public static boolean userIsMyFriend(String user) {
+        return User.getLoggedInUser().getFriends().contains(user);
     }
 
     public static void showRequestWindow(String from1) {
